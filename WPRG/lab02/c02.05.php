f780c0dfa0d06537a631e4121c2646183a5dbdc0
sugarvalley <cqr@o2.pl>
2021-03-17 16:29:46 +0100

    Added descriptions for tasks p02.04 and c02.05

--- WPRG/lab02/c02.05.php
+++ WPRG/lab02/c02.05.php
@@ -1,4 +1,13 @@
-<!--  -->
+<!-- PESEL decoder.
+
+> Variable $pesel = "PESEL_number";
+
+> Check if PESEL is correct (length and "check number") and show relevant information
+
+> If PESEL is correct:
+
+> Show sex and date of birth (dd-mm-yyyy) of the holder of the PESEL number -->
+
 <?php
 $pesel = "6701091688";
 $length = strlen($pesel);
--- WPRG/lab02/p02.04.php
+++ WPRG/lab02/p02.04.php
@@ -1,1 +1,23 @@
+<!-- Multiplication table
+
+> Show inside <pre></pre> multiplication table like below
+
+Example for $x=3:
+
+   | 1 | 2 | 3 |
+
+---+---+---+---+
+
+ 1 | 1 | 2 | 3 |
+
+---+---+---+---+
+
+ 2 | 2 | 4 | 6 |
+
+---+---+---+---+
+
+ 3 | 3 | 6 | 9 |
+
+---+---+---+---+  -->
+
 <?php
