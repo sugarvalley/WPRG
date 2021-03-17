bca907594a5160906ccb52c4a68310f9ddae83d4
sugarvalley <cqr@o2.pl>
2021-03-17 17:47:23 +0100

    Task p02.01 done

--- WPRG/lab02/p02.01.php
+++ WPRG/lab02/p02.01.php
@@ -7,3 +7,29 @@
 > Show median of the drawn numbers   -->
 
 <?php
+$numbers = array();
+$highest = 0;
+$counter = 1;
+$median = 0;
+
+for ($i = 0; $i < 100; $i++) {
+    $numbers[$i] = rand(0, 100);
+    if ($numbers[$i] > $highest) {
+        $highest = $numbers[$i];
+        $counter = 1;
+    } elseif ($numbers[$i] == $highest) {
+        $counter += 1;
+    }
+    if ($i != 99) {
+        echo $numbers[$i] . ", ";
+    } else {
+        echo $numbers[$i];
+    }
+}
+
+sort($numbers);
+$median = ($numbers[49] + $numbers[50]) / 2;
+
+echo "</br>The highest number is: " . $highest;
+echo "</br>Counter: " . $counter;
+echo "</br>Median: " . $median;
