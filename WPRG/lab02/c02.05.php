57a7110f6769550850a934109c76ec2305fb7e64
sugarvalley <cqr@o2.pl>
2021-03-20 15:27:44 +0100

    half of task c02.05 (pesel) done

--- WPRG/lab02/c02.05.php
+++ WPRG/lab02/c02.05.php
@@ -9,9 +9,32 @@
 > Show sex and date of birth (dd-mm-yyyy) of the holder of the PESEL number -->
 
 <?php
-$pesel = "6701091688";
+$pesel = "02070803628";
 $length = strlen($pesel);
 if ($length != 11) {
-    echo "PESEL " . $pesel . " jest niepoprawny!";
+    echo "PESEL " . $pesel . " jest niepoprawny! {wrong length}\n";
 } else {
+    $sum = 0;
+    $j = 1;
+    for ($i = 0; $i < $length-1; $i++) {
+        $number = (substr($pesel, $i, 1))*$j;
+        if ($number>=10) {
+            $sum+=substr($number, -1);
+        } else {
+            $sum+=$number;
+        }
+        $j+=2;
+        if ($j == 5) {
+            $j += 2;
+        } elseif ($j == 11) {
+            $j = 1;
+        }
+    }
+    $num = 10 - (substr($sum, -1));
+    $check = substr($pesel, -1);
+    if ($num != $check) {
+        echo "PESEL " . $pesel . " jest niepoprawny! {wrong number}";
+    } else {
+        echo "PESEL IS GOOD :)";
+    }
 }
