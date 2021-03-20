79f01c4875bd070ddc0d0c9be52e26ece6dc371a
sugarvalley <cqr@o2.pl>
2021-03-20 17:14:30 +0100

    last task done, few changes in other files

--- WPRG/lab02/p02.04.php
+++ WPRG/lab02/p02.04.php
@@ -30,17 +30,17 @@ for ($i = 1; $i <= $n; $i++) {
     echo " " . $i . " |";
 }
 echo "\n";
-for ($i = 1; $i <= $n+1; $i++) {
+for ($i = 1; $i <= $n + 1; $i++) {
     echo "---+";
 }
 echo "\n";
 for ($i = 1; $i <= $n; $i++) {
     echo " " . $i . " |";
     for ($j = 1; $j <= $n; $j++) {
-        echo " " . $j*$i . " |";
+        echo " " . $j * $i . " |";
     }
     echo "\n";
-    for ($k = 1; $k <= $n+1; $k++) {
+    for ($k = 1; $k <= $n + 1; $k++) {
         echo "---+";
     }
     echo "\n";
