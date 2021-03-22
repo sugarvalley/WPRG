3d3233a955041f4e8c0cac346eda3b1be0c58f2f
sugarvalley <cqr@o2.pl>
2021-03-22 17:01:25 +0100

    task01 done

---
+++ WPRG/lab03/c03.01.php
@@ -0,0 +1,31 @@
+<?php
+function circle($r)
+{
+    return pi() * ($r * $r);
+}
+
+function square($n)
+{
+    return $n * $n;
+}
+
+function rectangle($a, $b)
+{
+    return $a * $b;
+}
+
+function equilateralTriangle($n)
+{
+    return (($n * $n) * sqrt(3)) / 4;
+}
+
+function trapeze($a, $b, $h)
+{
+    return (($a+$b)*$h)/2;
+}
+
+echo circle(3) . "</br>";
+echo square(3) . "</br>";
+echo rectangle(3,4) . "</br>";
+echo equilateralTriangle(3) . "</br>";
+echo trapeze(3,4,2) . "</br>";
