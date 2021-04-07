bd1ebf8d710450ac121b31577f62b2853aed10ef
sugarvalley <cqr@o2.pl>
2021-04-07 17:38:17 +0200

    Last task done

---
+++ WPRG/lab04/p04.04.php
@@ -0,0 +1,51 @@
+<?php
+$shopping = file_get_contents('ToBuy');
+$prices = file_get_contents('Prices');
+$shopping = explode("\n", $shopping);
+$prices = explode("\n", $prices);
+$keys = array();
+$many = array();
+$products = array();
+$price = array();
+$s = array();
+$p = array();
+foreach ($shopping as $list) {
+    $list = explode(" ", $list);
+    $first = true;
+    foreach ($list as $value) {
+        if ($first) {
+            array_push($keys, $value);
+            $first = false;
+        } else {
+            array_push($many, $value);
+        }
+    }
+}
+$s = array_combine($keys, $many);
+foreach ($prices as $key) {
+    $key = explode(" ", $key);
+    $first = true;
+    foreach ($key as $product) {
+        if ($first) {
+            array_push($products, $product);
+            $first = false;
+        } else {
+            array_push($price, $product);
+        }
+    }
+}
+$p = array_combine($products, $price);
+$sum = 0;
+foreach ($s as $product => $value) {
+    foreach ($p as $name => $price) {
+        if ($product === $name) {
+            $temp = $value * $price;
+            echo $product . " " . $value . " x " . $price . " = " . $temp;
+            echo '</br>';
+            $sum += $temp;
+        }
+    }
+}
+echo '------------------------';
+echo '</br>';
+echo "&nbsp&nbspSUM &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" . $sum;
